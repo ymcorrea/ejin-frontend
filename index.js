9bf2d365f07e87a8c9782d5924a0bfbb7631828e
botbenz82 <botbenz111@gmail.com>
2021-10-31 18:47:09 +0600

    update status

--- index.js
+++ index.js
@@ -6,41 +6,47 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 
+// Connect cors and express json
 app.use(cors());
 app.use(express.json());
 
 // pass: gKMsQqXaDYVq0Rqq
 
+// Mongodb uri with username and password
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ha2x2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+// Create mongodb client
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+// Create run function for work with Mongodb
 async function run() {
     try {
+        // Connect client and make database and connection
         await client.connect();
         const database = client.db("travel_agency");
         const placeCollection = database.collection("destination");
         const serviceCollection = database.collection("resort");
         const bookCollection = database.collection("book");
 
-        // Place Collection API
+        // Get all Place Collection API
         app.get('/destination/', async (req, res) => {
             const cursor = await placeCollection.find({}).toArray();
             res.send(cursor);
         })
 
-        // Package Collection API
+        // Get all Package Collection API
         app.get('/package/', async (req, res) => {
             const cursor = await serviceCollection.find({}).toArray();
             res.send(cursor);
         })
 
-        // All booking Package get Collection API
+        // Get All booking Package Collection API
         app.get('/allbooking/', async (req, res) => {
             const booking = await bookCollection.find({}).toArray();
             res.send(booking);
         })
 
-        // Single Package get API 
+        // Get a Single Package API by id
         app.get('/package/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
@@ -48,7 +54,7 @@ async function run() {
             res.send(package);
         })
 
-        // Single booking Package API Delete 
+        // Delete a Single booking Package API 
         app.delete('/booking/:id', async (req, res) => {
             const id = req.params.id;
             const quary = { _id: ObjectId(id) };
@@ -67,48 +73,46 @@ async function run() {
                 },
             };
             const result = await bookCollection.updateOne(filter, updateDoc, options);
-        // console.log(id);
-        res.json(result);
-    })
+            // console.log(id);
+            res.json(result);
+        })
 
-    // Single Package API Post 
-    app.post('/users', async (req, res) => {
-        const user = req.body;
-        const result = await bookCollection.insertOne(user);
-        res.json(result);
-    })
+        // Post a Single Package to bookCollection API  
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await bookCollection.insertOne(user);
+            res.json(result);
+        })
 
-    // Add new Package API Post 
-    app.post('/package', async (req, res) => {
-        const package = req.body;
-        const result = await serviceCollection.insertOne(package);
-        res.json(result);
-    })
+        // Add new Package API Post 
+        app.post('/package', async (req, res) => {
+            const package = req.body;
+            const result = await serviceCollection.insertOne(package);
+            res.json(result);
+        })
 
-    // Get all booking from the database
-    app.get('/booking/', async (req, res) => {
-        const userEmail = req.query.email;
-        console.log(userEmail);
-        const quer = ({ Email: userEmail });
-        result = await bookCollection.find(quer).toArray();
-        console.log(result);
-        res.send(result);
-    })
+        // Get all booking from the database
+        app.get('/booking/', async (req, res) => {
+            const userEmail = req.query.email;
+            console.log(userEmail);
+            const quer = ({ Email: userEmail });
+            result = await bookCollection.find(quer).toArray();
+            console.log(result);
+            res.send(result);
+        })
 
 
-}
+    }
     finally {
-    // await client.close();
+        // await client.close();
+    }
 }
-}
+
+// Call run function
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send("Hello World")
-})
 
-
-
+// Set app listen running port
 app.listen(port, () => {
     console.log("running " + port)
 })
